6db40fe653f6c26dbb909f2690317280f6d6fce9
Shriyank.Soni <Shriyank.Soni@ril.com>
2025-07-05 21:02:30 +0530

    message api route fix

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -5,6 +5,6 @@ import { getUserForSideBar, sendMessage, getMessages } from '../controllers/mess
 const router = express.Router();
 
 router.get('/users', protectRoute, getUserForSideBar)
+router.get('/:id', protectRoute, getMessages); // better than '/:id'
 router.post('/send/:id', protectRoute, sendMessage)
-router.get('/messages/:id', protectRoute, getMessages); // better than '/:id'
 export default router
